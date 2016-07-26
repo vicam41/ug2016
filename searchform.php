e89e77a7847c17bedbc745aa524710071d44fe25
vicam41 <okay.amine@gmail.com>
2016-07-26 12:54:01 +0100

    Traduction

--- searchform.php
+++ searchform.php
@@ -8,7 +8,7 @@ $mts_options = get_option(MTS_THEME_NAME);
 ?><form method="get" id="searchform" class="search-form" action="<?php echo esc_attr( home_url() ); ?>" _lpchecked="1">
 	<fieldset>
 		<input type="text" name="s" id="s" value="<?php the_search_query(); ?>" placeholder="<?php _e('Search the site', 'schema' ); ?>" <?php if (!empty($mts_options['mts_ajax_search'])) echo ' autocomplete="off"'; ?> />
-		<button id="search-image" class="sbutton" type="submit" value="<?php _e('Search', 'schema'); ?>">Search
+		<button id="search-image" class="sbutton" type="submit" value="<?php _e('Search', 'schema'); ?>"><?php _e('Search', 'schema'); ?>
 	    </button>
 	</fieldset>
 </form>
